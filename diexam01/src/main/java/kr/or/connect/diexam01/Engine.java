d4f358128dcc7cf5e5f2b1abe418f503f6d6d683
JAEHYEON YOO <allan3129@gmail.com>
2021-11-18 15:49:27 +0900

    First Commit

---
+++ diexam01/src/main/java/kr/or/connect/diexam01/Engine.java
@@ -0,0 +1,15 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engine {
+	public Engine() {
+		System.out.println("Engine 생성자");
+	}
+	
+	public void exec() {
+		System.out.println("Engine이 동작합니다.");
+	}
+
+}
